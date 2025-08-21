651e7d1640907f16ffb545b33fd96d601e8f01c7
Abondance <kazadiabondance@gmail.com>
2025-08-21 16:37:01 +0200

    send my code

--- front/components/shared/Spinner.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Spinner() {
-  return (
-    <div className="fixed inset-0 flex justify-center items-center z-50">
-      <span className="loader"></span>
-    </div>
-  );
-}
